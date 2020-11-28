5e5baa79949cf081e02e02fcdf027dbb36cb351d
“chuajohn” <john@Johns-MacBook-Air-2.local>
2020-11-29 07:43:27 +0800

    in progress

--- App/app.R
+++ App/app.R
@@ -1,43 +1,57 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(tidyverse)
+library(shinythemes)
 
 
 data <- read_rds("shiny_data/data.rds")
+district_level <- read_rds("shiny_data/data_new.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    
+    shinythemes::themeSelector(),
     navbarPage("Food Insecurity in San Francisco",
-               tabPanel("Model",
-                        titlePanel("Racial Breakdown of SF Residents on Aid"),
+               tabPanel("The Neighborhood",
+                        titlePanel("Visualizing Needs in the Neighborhood of Alemany Farm"),
+                        
+                            # Show a plot of the generated distribution
+                            mainPanel(plotOutput("distPlot"), 
+                            p("The most common ethnic group represented among aid recipients is Hispanic"),
+                            plotOutput("histPlot"), 
+                             p("They are concentrated among low-income neighborhoods in SF."),
+                            plotOutput("newPlot"),
+                            leafletOutput("mapPlot")
+                            ))),
+               
+               
+               tabPanel("Demographics",
+                        titlePanel("By the Numbers: Assessing Needs in SF"),
                         
                         # Sidebar with a slider input for number of bins 
                         sidebarLayout(
-                            sidebarPanel(
-                                sliderInput("bins",
-                                            "Number of bins:",
-                                            min = 1,
-                                            max = 50,
-                                            value = 30)),
+                            sidebarPanel(selectInput("district_choice", "Supervisorial District", unique(district_level$supervisor_district)), 
+                                         selectInput("fiscal_year", "Fiscal Year", unique(district_level$fiscal_year)),
+                            selectInput(inputId = "y", 
+                                        label = "Y-axis:",
+                                        choices = c("Reading" = "read", "Writing" ="write", "Math" = "math", "Science" = "science", "Social Studies" = "socst"), 
+                                        selected = "math")),
+                            
                             
                             # Show a plot of the generated distribution
-                            mainPanel(plotOutput("distPlot")))),
+                            mainPanel(plotOutput("distPlot"), 
+                                      p("The most common ethnic group represented among aid recipients is Hispanic"),
+                                      plotOutput("histPlot"), 
+                                      p("They are concentrated among low-income neighborhoods in SF."),
+                                      plotOutput("newPlot"),
+                                      leafletOutput("mapPlot")
+                            ))),
+               
                
                tabPanel("About",
                         titlePanel("About"),
                         br(),
                         h4("About the data"),
-                        p("This dataset is from the SF Mayor's Office of Housing and Community Development, which contains data about low-income SF residents participating in the Community Development Public Service Programs. The plot is a simple visualization of the racial composition of these recipients. I will definitely be playing around with the data more to create different types of plots. 
-"),
+                        p("This dataset is from the SF Mayor's Office of Housing and Community Development, which contains data about low-income SF residents participating in the Community Development Public Service Programs. The plot is a simple visualization of the racial composition of these recipients. I will definitely be playing around with the data more to create different types of plots."),
                         br(),
                         h4("About me"),
                         p("I am a student at Harvard College studying Gov."),
@@ -52,9 +66,38 @@ server <- function(input, output) {
     output$distPlot <- renderPlot({
         data %>%
             ggplot(aes(x = race_ethnicity)) +
-            geom_bar()
+            geom_bar() +
+            labs(title = "SF Mayor's Office Aid Recipients According to Racial Group", x = "Race/Ethnicity", y = "Number of Individuals") +
+            theme_minimal()
+        })
+    
+    output$histPlot <- renderPlot({
+        data %>%
+            ggplot(aes(x = neighborhood)) +
+            geom_bar() +
+            labs(title = "SF Mayor's Office Aid Recipients According to Neighborhood", x = "Neighborhood", y = "Number of Individuals") +
+            coord_flip() +
+            theme_minimal()
     })
-
+    
+    output$newPlot <- renderPlot({
+        district_level %>%
+            filter(supervisor_district == input$district_choice) %>%
+            filter(fiscal_year == input$fiscal_year) %>%
+            ggplot(aes(x = race_ethnicity)) +
+            geom_bar() +
+            labs(title = paste("SF Mayor's Office Aid Recipients According to Race In District ", input$district_choice), x = "Race/Ethnicity", y = "Number of Individuals") +
+            coord_flip() +
+            theme_minimal()
+    })
+    
+    output$mapPlot <- renderLeaflet({
+        
+        
+        
+        
+    })
+    
 }
 
 # Run the application 
